091551a92d714b757f2f884be33e202c8bfd93df
Omofon <abasiomofonudoh@gmail.com>
2025-11-08 23:58:36 +0100

    contd day6

---
+++ day6/debug.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function test(label, body) {
+  if (!body()) console.log(`Failed: ${label}`);
+}
+
+test("convert Latin text to uppercase", () => {
+  return "hello".toUpperCase() == "HELLO";
+});
+
+function numberToString(n, base = 10) {
+  let result = "",
+    sign = "";
+  if (n < 0) {
+    sign = "-";
+    n = -n;
+  }
+  do {
+    result = String(n % base) + result;
+    n /= base;
+  } while (n > 0);
+  return sign + result;
+}
+
+function promptNumber(question) {
+  let result = Number(prompt(question));
+  if (Number.isNaN(result)) return null;
+  else return result;
+}
