e63a524b376deeb50c90594431737e8e449c4f8c
Abasiomofon Udoh <omofon.u@gmail.com>
2025-11-10 12:24:48 +0100

    Done debug

--- day6/debug.js
+++ day6/debug.js
@@ -52,3 +52,47 @@ function reliableMultiply(a, b) {
 }
 
 console.log(reliableMultiply(8, 8));
+
+// Box Challenge
+const box = {
+  locked: true,
+  unlock() {
+    this.locked = false;
+  },
+  lock() {
+    this.locked = true;
+  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  },
+};
+
+function withBoxUnlocked(body) {
+  const wasLocked = box.locked;
+  if (!wasLocked) {
+    return body();
+  }
+
+  box.unlock();
+  try {
+    return body();
+  } finally {
+    box.lock();
+  }
+}
+
+withBoxUnlocked(function () {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function () {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised:", e.message);
+}
+
+console.log(box.locked);
