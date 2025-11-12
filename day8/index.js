9f336b105dafea82ea2bdd5d329c3d39dd2e1f62
Abasiomofon Udoh <omofon.u@gmail.com>
2025-11-12 05:40:16 +0100

    initial exercises for day8

--- day8/index.js
+++ day8/index.js
@@ -1,1 +1,12 @@
-let link = document.getElementsByTagName("a")
+let link = document.getElementsByTagName("a");
+
+function replaceImages() {
+  let images = document.body.getElementsByTagName("img");
+  for (let i = images.length - 1; i >= 0; i--) {
+    let image = images[i];
+    if (image.alt) {
+        let text = document.createTextNode(image.alt)
+        image.parentNode.replaceChild(text, image)
+    }
+  }
+}
